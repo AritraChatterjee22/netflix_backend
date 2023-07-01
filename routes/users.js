24860f0c28c21ae6ff3ee7937788fd59b4175cf2
Aritra <chatterjee.aritra@protonmail.com>
2023-07-02 00:55:21 +0530

    First commit

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+
+//UPDATE
+router.put("/:id", async(req,res)=>{
+
+})
+
+
+
+
+//DELETE
+//GET ONE
+//GET ALL 
+//GET USER STATS 
