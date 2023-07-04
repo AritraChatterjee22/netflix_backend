77f91790b31ce42306a9d28a025676526f8716ec
Aritra <chatterjee.aritra@protonmail.com>
2023-07-04 16:29:02 +0530

    users routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,33 @@
 const router = require("express").Router();
+const verify  = require("../verifyToken");
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 
 
 //UPDATE
-router.put("/:id", async(req,res)=>{
+router.put("/:id", verify, async(req,res)=>{
+  if(req.user.id === req.params.id || req.user.isAdmin){
+    if(req.body.password){
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+      ).toString();
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        { 
+          $set:req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  } else{
+    res.status(403).json("You can only update your account")
+  } 
 
 })
 
@@ -12,6 +35,58 @@ router.put("/:id", async(req,res)=>{
 
 
 //DELETE
+
+router.delete("/:id", verify, async(req,res)=>{
+  if(req.user.id === req.params.id || req.user.isAdmin){
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("user has been deleted...");
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  } else{
+    res.status(403).json("you can only delete your account")
+  } 
+
+})
+
+
 //GET ONE
+
+router.get("/find/:id", async(req,res)=>{
+    try {
+      const user = await User.findById(req.params.id);
+      const {password, ...info} = user._doc; 
+      res.status(200).json(info);
+    } catch (err) {
+      res.status(500).json(err)
+    }
+});
+
+
+
 //GET ALL 
+
+router.get("/", verify, async(req,res)=>{
+  const query = req.query.new;
+  if(req.user.isAdmin){
+    try {
+      const users = query ? await User.find().sort({_id: -1}).limit(5) : await User.find(); 
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  } else{
+    res.status(403).json("you are not allowed to see all users")
+  } 
+
+})
+
+
+
 //GET USER STATS 
+
+
+
+
+module.exports = router
